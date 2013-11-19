cc4964dedc89e1c8a3f2fb7a3617cb02ee7d64ec
Sen Zhang <sen.zhang@appfolio.com>
2013-11-18 19:32:48 -0800

    sz - add a movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20131119031812) do
+
+  create_table "movies", :force => true do |t|
+    t.string   "title"
+    t.string   "rating"
+    t.decimal  "total_gross", :precision => 10, :scale => 0
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
+  end
 
 end
