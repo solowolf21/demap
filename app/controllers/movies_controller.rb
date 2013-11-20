5d0567655e9bbf1530710890424ffb139f57e919
Sen Zhang <sen.zhang@appfolio.com>
2013-11-19 22:04:06 -0800

    sz - pull in simple_form and strong_params, enable edit function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,14 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update_attributes(params[:movie].permit(:title, :description, :rating, :released_on, :total_gross))
+    redirect_to @movie
+  end
 end
