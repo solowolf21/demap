78f8de049830bf59127e7e14da23145cce724204
Sen Zhang <sen.zhang@appfolio.com>
2013-11-19 17:34:37 -0800

    sz - improve some tests

--- test/unit/movie_test.rb
+++ test/unit/movie_test.rb
@@ -7,4 +7,21 @@ class MovieTest < ActiveSupport::TestCase
     movie.update_attributes!(:total_gross => 99999999999)
     assert !movie.flop?
   end
+
+  def test_setter_getter
+    movie = Movie.create_exemplar!
+    movie.title       = 'Flix'
+    movie.rating      = 'PG-88'
+    movie.description = 'The movie is fair.'
+    movie.released_on = '2011-08-06'
+    movie.total_gross = 7777777
+    movie.save!
+
+    movie.reload
+    assert_equal 'Flix', movie.title
+    assert_equal 'PG-88', movie.rating
+    assert_equal 'The movie is fair.', movie.description
+    assert_equal 'Sat, 06 Aug 2011', movie.released_on.inspect
+    assert_equal 7777777, movie.total_gross
+  end
 end
