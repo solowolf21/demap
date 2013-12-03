5eacad02d4b2a882a2515a70f2cd10be28b871fc
Sen Zhang <sen.zhang@appfolio.com>
2013-12-03 10:43:02 -0800

    sz - enable sign in, sign up and logout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131126032144) do
+ActiveRecord::Schema.define(:version => 20131130034802) do
 
   create_table "movies", :force => true do |t|
     t.string   "title"
@@ -36,5 +36,23 @@ ActiveRecord::Schema.define(:version => 20131126032144) do
   end
 
   add_index "reviews", ["movie_id"], :name => "index_reviews_on_movie_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
